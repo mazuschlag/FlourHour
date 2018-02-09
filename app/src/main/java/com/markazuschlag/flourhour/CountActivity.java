d6e17e19df97dbfdf6ea1381e8fdce4e846516e7
mazuschlag <markazuschlag@gmail.com>
2018-02-09 15:40:32 -0800

    Now utilizes onSaveInstanceState

--- app/src/main/java/com/markazuschlag/flourhour/CountActivity.java
+++ app/src/main/java/com/markazuschlag/flourhour/CountActivity.java
@@ -1,5 +1,6 @@
 package com.markazuschlag.flourhour;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MotionEvent;
@@ -11,9 +12,15 @@ import static com.markazuschlag.flourhour.MainActivity.EXTRA_INCREMENT_BY;
 import static com.markazuschlag.flourhour.MainActivity.EXTRA_TARGET;
 
 public class CountActivity extends AppCompatActivity {
+    public static final String SAVE_COUNT = "com.markazuschlag.flourhour.save.COUNT";
+    public static final String SAVE_DESCRIPTION = "com.markazuschlag.flourhour.save.DESCRIPTION";
     private TextView mCountText;
+    private TextView mDescriptionText;
     private ImageView mCountButton;
     private int mCount;
+    private String mTarget;
+    private String mIncrementBy;
+    private String mDescription;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,18 +28,29 @@ public class CountActivity extends AppCompatActivity {
         setContentView(R.layout.activity_count);
 
         mCountText = findViewById(R.id.countText);
+        mDescriptionText = findViewById(R.id.descriptionTextView);
         mCountButton = findViewById(R.id.countButton);
 
         Bundle extras = getIntent().getExtras();
-        if (extras != null) {
-            String mTarget = extras.getString(EXTRA_TARGET);
-            String mIncrementBy = extras.getString(EXTRA_INCREMENT_BY);
+        if (savedInstanceState != null) {
+            mCount = savedInstanceState.getInt(SAVE_COUNT);
+            mTarget = String.valueOf(mCount);
+            mIncrementBy = savedInstanceState.getString(SAVE_DESCRIPTION);
+
+        } else if (extras != null) {
+            mTarget = extras.getString(EXTRA_TARGET);
             mCount = Integer.parseInt(mTarget);
-            mCountText.setText(mTarget);
+            mIncrementBy = extras.getString(EXTRA_INCREMENT_BY);
+
         } else {
-            mCount = 0;
+            mCount = 10;
+            mTarget = String.valueOf(mCount);
+            mIncrementBy = getString(R.string.default_increment);
         }
 
+        mDescription = mIncrementBy + getString(R.string.cups_left);
+        mDescriptionText.setText(mDescription);
+        mCountText.setText(mTarget);
 
         mCountButton.setOnTouchListener(new View.OnTouchListener() {
             @Override
@@ -44,6 +62,9 @@ public class CountActivity extends AppCompatActivity {
                         break;
                     case MotionEvent.ACTION_UP:
                         mCountButton.setImageResource(R.drawable.ic_no_touch);
+                        if (mCount <= 0) {
+                            endCounting();
+                        }
                         break;
                     default:
                         break;
@@ -51,5 +72,18 @@ public class CountActivity extends AppCompatActivity {
                 return true;
             }
         });
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt(SAVE_COUNT, mCount);
+        outState.putString(SAVE_DESCRIPTION, mIncrementBy);
+
+    }
+
+    public void endCounting() {
+        Intent intent = new Intent(this, FinishActivity.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/markazuschlag/flourhour/MainActivity.java
+++ app/src/main/java/com/markazuschlag/flourhour/MainActivity.java
@@ -11,8 +11,10 @@ import android.widget.EditText;
 import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
-    public static final String EXTRA_TARGET = "com.markazuschlag.flourhour.TARGET";
-    public static final String EXTRA_INCREMENT_BY = "com.markazuschlag.flourhour.increment.BY";
+    public static final String EXTRA_TARGET = "com.markazuschlag.flourhour.extra.TARGET";
+    public static final String EXTRA_INCREMENT_BY = "com.markazuschlag.flourhour.extra.INCREMENTBY";
+    public static final String SAVE_TARGET = "com.markazuschlag.flourhour.save.TARGET";
+    public static final String SAVE_INCREMENT_BY = "com.markazuschlag.flourhour.save.INCREMENTBY";
     private EditText mTargetEditText;
     private Spinner mIncrementSpin;
     private String mTarget;
@@ -27,7 +29,7 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
 
         mIncrementSpin = findViewById(R.id.incrementSpinner);
         if (mIncrementSpin != null) {
-            mIncrementSpin.setOnItemSelectedListener((AdapterView.OnItemSelectedListener) this);
+            mIncrementSpin.setOnItemSelectedListener(this);
         }
 
         ArrayAdapter<CharSequence> incrementAdapt = ArrayAdapter.createFromResource(this,
@@ -37,6 +39,14 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
 
         if (mIncrementSpin != null) {
             mIncrementSpin.setAdapter(incrementAdapt);
+        }
+
+        if (savedInstanceState != null) {
+            String currentTarget = savedInstanceState.getString(SAVE_TARGET);
+            int mIncrementIndex = savedInstanceState.getInt(SAVE_INCREMENT_BY);
+
+            mTargetEditText.setText(currentTarget);
+            mIncrementSpin.setSelection(mIncrementIndex);
         }
     }
 
@@ -48,7 +58,14 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
 
     @Override
     public void onNothingSelected(AdapterView<?> adapterView) {
+        // Nothing to see here
+    }
 
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putString(SAVE_TARGET, mTargetEditText.getText().toString());
+        outState.putInt(SAVE_INCREMENT_BY, mIncrementSpin.getSelectedItemPosition());
     }
 
     public void startCounting(View view) {
